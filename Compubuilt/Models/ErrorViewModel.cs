452b40049f372ef96b3d8a03ec0ecd108a5c7cb5
Bartosz Kiliś <b0rt3k@windowslive.com>
2022-05-24 22:24:03 +0200

    Add project files.

---
+++ Compubuilt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Compubuilt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
